01f1a3671e4648ecea2847dfde8024f8f70ff109
KARTIKEY-KATYAL <107783109+KARTIKEY-KATYAL@users.noreply.github.com>
2024-09-26 19:39:16 +0530

    second commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,34 +1,49 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { Button } from './ui/button'
-import { PenBox } from 'lucide-react'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { PenBox } from "lucide-react";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import UserMenu from "./UserMenu";
+
 const Header = () => {
   return (
-    <nav className="mx-auto py-2 px-4 flex justify-between bg-slate-400 shadow-md bottom-2">
+    <nav className="mx-auto py-2 px-4 flex justify-between items-center bg-slate-400 shadow-md">
       <Link href="/" className="flex items-center">
         <Image
           src="/logo.png"
           width={150}
           height={60}
-          alt="logo"
+          alt="MeetMate Logo"
           className="h-16 w-auto"
-        ></Image>
+        />
       </Link>
-      <div className='flex gap-2 items-center justify-center'>
-        <Link href={"/events?create=true"}>
-          <Button variant="link" className="bg-green-950 font-semibold text-yellow-300">
-            <PenBox size={18}></PenBox>
+      <div className="flex gap-4 items-center">
+        <Link href="/events?create=true">
+          <Button
+            variant="link"
+            className="bg-green-950 font-semibold text-yellow-300 flex items-center gap-1"
+          >
+            <PenBox size={18} aria-hidden="true" />
             Create an Event
           </Button>
         </Link>
-
-        <Button variant="outline" className="bg-blue-700 font-semibold  hover:bg-blue-600 text-white">
-          Log In
-        </Button>
+        <SignedOut>
+          <SignInButton redirectUrl="/dashboard">
+            <Button
+              variant="outline"
+              className="bg-blue-700 font-semibold hover:bg-blue-600 text-white"
+            >
+              Log In
+            </Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserMenu />
+        </SignedIn>
       </div>
     </nav>
   );
-}
+};
 
-export default Header
+export default Header;
